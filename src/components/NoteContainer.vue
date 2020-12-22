711baf37d2e51215f64de9395aa9adb74afecdb2
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-12-21 21:11:13 -0500

    add note selector

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="note-container">
+       <NoteSelectors />  
+      
+    </div>
+</template>
+
+<script>
+import NoteSelectors from "./NoteSelectors.vue";
+export default {
+  components: {
+    NoteSelectors,
+  },
+  //  const app = Vue.createApp({
+  //   data(){
+  //     return {count: 3}
+  //   },
+  // })
+};
+</script> 
